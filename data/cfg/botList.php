f5f3a922bd406c9dd361db1e5224222a996cff7d
Anatolii A. Apanasiuk <a3three@gmail.com>
2018-05-03 21:54:06 +0200

    a few initial stuff were added

---
+++ data/cfg/botList.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tolya
+ * Date: 10.04.2018
+ * Time: 21:47
+ */
+
+return array(
+    'Googlebot',
+    'Mediapartners-Google',
+    'APIs-Google',
+    'AdsBot-Google',
+    'AdsBot-Google-Mobile',
+    'Googlebot-Image',
+    'Googlebot-News',
+    'Googlebot-Video',
+    'AdsBot-Google-Mobile-Apps',
+    'YandexBot',
+    'YandexDirect',
+    'YandexImages',
+    'YandexMetrika',
+    'YandexMobileBot',
+    'YandexMedia',
+    'YandexNews',
+    'YandexPagechecker',
+    'YandexMarket',
+    'YandexCalenda',
+    'YandexDirectDyn',
+    'YaDirectFetcher',
+    'YandexAccessibilityBot',
+    'YandexScreenshotBot',
+    'YandexVideoParser',
+    'YandexSearchShop',
+    'YandexOntoDBAPI',
+    'Baiduspider',
+    'Cliqzbot',
+    'AhrefsBot',
+    'Genieo',
+    'Bingbot',
+    'Slurp',
+    'DuckDuckBot',
+    'facebot',
+    'WebAlta',
+    'BomboraBot',
+    'CCBot',
+    'MSNBot',
+    'Mail.Ru',
+    'ia_archiver',
+    'Teoma',
+    'mauibot',
+    'dnyzbot'
+);
---
+++ data/cfg/config.php
@@ -0,0 +1,24 @@
+<?php 
+return array(
+    'db' => array(
+        'host' => 'localhost',
+        'user' => '',
+        'pass' => '',
+        'name' => '',
+    ),
+    'site' => array(
+        'url'     => 'http://lucky-dress.eu/',
+    	'defLang' => 'pl',
+        'imgIndex' => '/data/imgIndex.csv',
+    ),
+    'stat' => array(
+        'OK_mail'   => '/data/ok_requests.txt',
+        'FAIL_mail' => '/data/bad_requests.txt',
+        'hits'      => '/data/hitcount.txt',
+    ),
+    'mediaDirs' => array(
+            "other"=>"_other",
+             "misc"=>"_design",
+              "g18"=>"2018",
+              "g17"=>"2017"),
+);
